f96c1c19c8133033a9679c02440cb6a5a320081b
DNS <dalaymongush@gmail.com>
2026-05-29 13:32:32 +0700

    fix: dotenv only in development

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const path = require("path");
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const categoryRoutes = require("./routes/categoryRoutes");
 const productRoutes = require("./routes/productRoutes");
