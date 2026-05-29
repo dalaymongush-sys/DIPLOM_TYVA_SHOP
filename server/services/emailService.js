3155715336bc1313566258d76c154f8221e5c674
DNS <dalaymongush@gmail.com>
2026-05-29 22:04:53 +0700

    fix: use tuvashop.ru domain for emails

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,7 +1,7 @@
 ﻿const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const FROM = 'onboarding@resend.dev';
+const FROM = 'noreply@tuvashop.ru';
 const NAME = 'Tuva Sport Shop';
 
 const sendVerificationCode = async (email, code) => {
