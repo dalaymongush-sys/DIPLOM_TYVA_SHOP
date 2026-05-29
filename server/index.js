d94d5a8f04e8fb3ec9d2090bb2b3619459815af6
DNS <dalaymongush@gmail.com>
2026-05-29 16:49:12 +0700

    fix: trust proxy for Railway

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const settingsRoutes = require("./routes/settingsRoutes");
 const promoRoutes = require("./routes/promoRoutes");
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 app.use(helmet({
