9580eed9157006b762e3cf6c9bc80707386e11c8
DNS <dalaymongush@gmail.com>
2026-05-29 14:06:00 +0700

    fix: allow all hosts in vite preview

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'gleaming-success-production.up.railway.app',
+      'tuvashop.ru',
+      'www.tuvashop.ru',
+    ],
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+  },
+  server: {
+    host: '0.0.0.0',
+  }
 })
